de11b17824f099decf652bfe5ef270f81d14b5fc
rajneesh0108 <109803369+rajneesh0108@users.noreply.github.com>
2023-07-19 14:28:56 +0530

    Create main.tf

---
+++ vm/main.tf
@@ -0,0 +1,13 @@
+resource "google_compute_instance" "vms" {
+  count = var.num_vms
+
+  name = "vm-${count.index}"
+  machine_type = var.machine_types[count.index]
+  zone = var.zones[count.index]
+
+  boot_disk {
+    initialize_params {
+      image = var.images[count.index]
+    }
+  }
+}
