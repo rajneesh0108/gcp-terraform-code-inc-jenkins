da08fe5267b2c04d182441f107634c9bd9da683f
rajneesh0108 <109803369+rajneesh0108@users.noreply.github.com>
2023-07-19 16:57:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,12 +11,10 @@ module "vms" {
   machine_types = var.machine_types
   images = var.images
   zones = var.zones
-
-  # The `vm_ids` and `vm_names` variables are defined as outputs of the
-  # `module.vms` block.
-  local.vm_ids = module.vms.vm_ids
-  local.vm_names = module.vms.vm_names
 }
 
-# The `locals` block defines two local variables, `vm_ids` and `vm_names`,
-# which are the outputs of the `module.vms` block.
+locals {
+  vm_ids = module.vms.vm_ids
+  vm_names = module.vms.vm_names
+
+}
