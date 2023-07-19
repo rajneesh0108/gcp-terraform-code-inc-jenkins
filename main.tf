df8fc335d3551fd80425e57182ab179054461f13
rajneesh0108 <109803369+rajneesh0108@users.noreply.github.com>
2023-07-19 16:32:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,15 +13,14 @@ module "vms" {
   images = var.images
   zones = var.zones
 
-  # The `vpc_id` and `subnet_id` variables are defined as outputs of the
-  # `module.vpc` block.
   vpc_id = module.vpc.outputs.id
   subnet_id = module.vpc.outputs.subnet_id
 
-  # The `locals` block defines two local variables, `vm_ids` and `vm_names`,
-  # which are the outputs of the `module.vms` block.
-  locals {
-    vm_ids = module.vms.vm_ids
-    vm_names = module.vms.vm_names
+  output "vm_ids" {
+    value = module.vms.vm_ids
+  }
+
+  output "vm_names" {
+    value = module.vms.vm_names
   }
 }
