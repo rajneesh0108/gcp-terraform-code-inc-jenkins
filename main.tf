440a49b5c0782162237cf59135eb2a0e57aa347a
rajneesh0108 <109803369+rajneesh0108@users.noreply.github.com>
2023-07-19 16:39:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,16 +6,16 @@ module "vpc" {
 
 module "vms" {
   source = "./modules/vm"
-  depends_on = [module.vpc]
 
   num_vms = var.num_vms
   machine_types = var.machine_types
   images = var.images
   zones = var.zones
+}
 
-
-  locals {
-    vm_ids = module.vms.vm_ids
-    vm_names = module.vms.vm_names
-  }
+locals {
+  vm_ids = module.vms.vm_ids
+  vm_names = module.vms.vm_names
+  vpc_id = module.vpc.id
+  subnet_id = module.vpc.subnet_id
 }
